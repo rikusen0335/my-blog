4163316bd45d686d982a685119f45323ab065097
rikusen0335 <rikusen0335@gmail.com>
2020-07-19 19:15:01 +0900

    delete: resource blog contents, add: first article

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -21,7 +21,6 @@ export default () => (
           className="primary-content" 
           dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
         />
-        <Link to='/contact' className="button -primary">Get in touch &rarr;</Link>
       </div>
     )}
   />
